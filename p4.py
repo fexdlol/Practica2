308331808f2df7cf6f5a5b76c1e2c8b369e7ed5e
Fernando Paredes <fe_paredesm@hotmail.com>
2019-11-20 08:29:00 -0500

    Actualizado

--- p4.py
+++ p4.py
@@ -3,9 +3,23 @@ import random
 os.system("cls")
 
 tamaño = int(input("Ingrese el tamaño:"))
-if(tamaño>=2 and tamaño<=5):
-    numero = numero.randint(1,100)
-else:
-    tamaño = int(input("Ingrese el tamaño:"))
-matriz = [tamaño][tamaño]
+while True:
+    if(tamaño>=2 and tamaño<=5):
+        break
+    else:
+        tamaño = int(input("Ingrese el tamaño:"))
+matriz = []
+for i in range(0, tamaño):
+    matriz.append([])
+for i in range(0, tamaño):
+    matriz[0].append(random.randint(1,100))
+for i in range(0, tamaño):
+    matriz[1].append(random.randint(1,100))
+for i in range(0, tamaño):
+    matriz[2].append(random.randint(1,100))
+for i in range(0, tamaño):
+    matriz[3].append(random.randint(1,100))
+for i in range(0, tamaño):
+    matriz[4].append(random.randint(1,100))
 
+print(matriz)
