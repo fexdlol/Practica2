3827bc03acd57920bda0ff472f1477bbcc98b672
Fernando Paredes <fe_paredesm@hotmail.com>
2019-11-20 08:07:57 -0500

    Actualizado

---
+++ P2.py
@@ -0,0 +1,8 @@
+import os
+os.system("cls")
+
+def contarletra (Texto, letra):
+    for letra in Texto:
+        if letra.count(Texto):
+            print(letra)
+        return contarletra
---
+++ P3.py
@@ -0,0 +1,34 @@
+import os
+os.system("cls")
+
+n = []
+s = []
+e = []
+a = 0
+b = 0
+
+while len(n) < 5:
+    Nombre = input("Ingrese su nombre:")
+    n.append(Nombre)
+while len(s) < 5:
+    Sexo = input("Ingrese su sexo:")
+    s.append(Sexo)
+    if Sexo == "Masculino":
+        a += 1
+    if Sexo == "Femenino":
+        b += 1
+while len(e) < 5:
+    Edad = int(input("Ingrese su edad:"))
+    if Edad>=5 and Edad<=17:
+        e.append(Edad)
+    else:
+        print("Persona no apta")
+promedio = (e[0]+e[1]+e[2]+e[3]+e[4])/5      
+
+print("Lista de registro")
+print(n)
+print(s)
+print(e)
+print("Hay", a, "hombres")
+print("Hay", b, "mujeres")
+print(promedio)
