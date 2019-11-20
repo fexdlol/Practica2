29a125b91728c2bc86006404c1d25bae92f4cddf
Fernando Paredes <fe_paredesm@hotmail.com>
2019-11-20 08:37:53 -0500

    Actualizado

--- P2.py
+++ P2.py
@@ -1,11 +1,8 @@
 import os
 os.system("cls")
 
-A=["H","o","l","a","  ","M","u","n","d","o"]
-letra = input("Escriba la letra:")
-A.index(letra)
-print(letra)
-B=[1,2,3,1,1,3,3,4,2,1]
-numero = int(input("Eliga el numero:"))
-posicion = B.index(numero)
-print(B[posicion])
+def contarletra (Texto, letra):
+    for letra in Texto:
+        if letra.count(Texto):
+            print(letra)
+    return contarletra
