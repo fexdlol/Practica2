d5c48f7d444429163f9cf93e6b5c232623d5dc46
Fernando Paredes <fe_paredesm@hotmail.com>
2019-11-20 08:32:11 -0500

    Actualizado

--- P2.py
+++ P2.py
@@ -1,9 +1,11 @@
 import os
 os.system("cls")
 
-A=["H","o","l","a"," ","M","u","n","d","o"]
+A=["H","o","l","a","  ","M","u","n","d","o"]
 letra = input("Escriba la letra:")
 A.index(letra)
 print(letra)
 B=[1,2,3,1,1,3,3,4,2,1]
 numero = int(input("Eliga el numero:"))
+posicion = B.index(numero)
+print(B[posicion])
